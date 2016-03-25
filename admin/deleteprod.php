25a2b040517482da1f75e9104f869df4fd978008
Shobi P P <ppshobi@gmail.com>
2016-03-25 17:41:38 +0530

    Added Delete product functionality in a blank page with login check

--- admin/deleteprod.php
+++ admin/deleteprod.php
@@ -1,4 +1,42 @@
-<?php
+<?php session_start();
+include_once("../includes/dbconn.php");
+include_once("adminfunctions.php");
 
+if(isadminloggedin()){
+    //do nothing stay here
+}
+else{
+    header("location:login.php");
+}
+$seller=$_SESSION['farmercart_admin_id'];
+$prodid="";
+if (isset($_GET['prodid'])) {
+	$prodid=$_GET['prodid'];
+}
+
+$sql="DELETE FROM product WHERE id=$prodid";
+$result=mysqlexec($sql);
+if ($result) {
+	echo "Product Deleted Successfully";
+}else{
+	echo "Product Deletion failed";
+}
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete Product- Farmercart</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+
+</head>
+<body>
+<a href="viewproducts.php">
+<button type="button" class="btn btn-success">Back To view Products</button>
+</a>
+</body>
+</html>
